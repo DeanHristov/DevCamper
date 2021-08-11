ee42ca109933336c4dbb1a9de42675bde9728473
D. Hristov <dean.hristov89@gmail.com>
2021-08-11 15:54:42 +0300

    [CORE][DB] Adding connection to MongoDB

---
+++ src/core/connectDB.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const colors = require('colors');
+module.exports = async (MONGO_URI) => {
+    console.log(colors.yellow('Connecting to MongoDB....'))
+    const connectDB = await mongoose.connect(MONGO_URI, {
+       useNewUrlParser: true,
+       useCreateIndex: true,
+       useFindAndModify: false,
+       useUnifiedTopology: true,
+    });
+
+    console.log(colors.green(`MongoDB is connected to host: ${connectDB.connection.host.bold.underline}`))
+}
+
+
+
+
+
+
+
+
--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+
+
+const connectDB = require('./core/connectDB');
 
 // Load config vars
 dotenv.config();
 const {
-    API_VERSION, NODE_ENV, NODE_PORT
+    API_VERSION, NODE_ENV, NODE_PORT,
+    MONGO_URI
 } = process.env;
 
+// Connected to DB:
+connectDB(MONGO_URI)
 // Creates an Express application
 const app = express();
 const PORT = NODE_PORT || 3000
@@ -25,7 +32,11 @@ const bootcamps = require('./routes/bootcamps');
 app.use(`${API_VERSION}/bootcamps`, bootcamps);
 
 // Running the server
-app.listen(PORT, () => console.log(
-    `The server is running in "${NODE_ENV}" mode on port ${PORT}`
+const server = app.listen(PORT, () => console.log(
+    colors.green(`The server is running in "${NODE_ENV.underline}" mode on port ${PORT.underline}`)
 ))
 
+process.on('unhandledRejection', async (reason, promise) => {
+    console.log(colors.bgRed(`Error! ${reason.message}`));
+    server.close(() => process.exit(1))
+})
