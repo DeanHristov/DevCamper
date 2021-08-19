8177accbf149ec89c398284025bc455722e8a5eb
D. Hristov <dean.hristov89@gmail.com>
2021-08-19 11:30:56 +0300

    [AUTH][USERS] - Included functionality to modify user details and change the password

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const {
     registerNewUser, signIn, getMyself,
-    forgotPassword, resetPassword
+    forgotPassword, resetPassword, modifyUser,
+    updateUserPassword,
 } = require("../controllers/auth");
 
 const { protectRoute } = require('../middlewares/auth')
@@ -12,6 +13,8 @@ router.post('/register', registerNewUser)
 router.post('/signIn', signIn)
 router.post('/forgot-password', forgotPassword)
 router.put('/reset-password/:resetToken', resetPassword)
+router.put('/update-users', protectRoute, modifyUser)
+router.put('/change-password', protectRoute, updateUserPassword)
 router.get('/me', protectRoute, getMyself)
 
 module.exports = router;
